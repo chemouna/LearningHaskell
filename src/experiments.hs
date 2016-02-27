fde8ab0c6832659a413c5b637c37234edeae0920
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-02-27 10:48:45 +0100

    More expls.

--- src/experiments.hs
+++ src/experiments.hs
@@ -16,3 +16,21 @@ chain n
 numLongChains :: Int
 numLongChains = length (filter isLong (map chain [1..100]))
     where isLong xs = length xs > 15
+
+numLongChainsWithLambda :: Int
+numLongChainsWithLambda = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+sum2' :: (Num a) => [a] -> a
+sum2' = foldl (+) 0
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
+
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr (\x acc -> f x : acc) [] xs
+
+mapWithFoldl' :: (a -> b) -> [a] -> [b]
+mapWithFoldl' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
