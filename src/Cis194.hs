ca5ccbadf1638ed722fe0a65d0006598cadb0f8c
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-04-02 16:39:00 +0200

    Parts of exercices in CIS194 course.

---
+++ src/Cis194.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Cis194 where
+
+gt100 :: Integer -> Bool
+gt100 x = x > 100
+
+greaterThan100 :: [Integer] -> [Integer]
+greaterThan100 xs = filter gt100 xs
+
+greaterThan100_2 :: [Integer] -> [Integer]
+greaterThan100_2 xs = filter (\x -> x > 100) xs
+
+greaterThan100_3 :: [Integer] -> [Integer]
+greaterThan100_3 xs = filter (> 100) xs
+
+myTest :: [Integer] -> Bool
+myTest xs = even (length (greaterThan100 xs))
+
+myTest' :: [Integer] -> Bool
+myTest' = even . length . greaterThan100
+
+foobar :: [Integer] -> Integer
+foobar []     = 0
+foobar (x:xs)
+  | x > 3     = (7*x + 2) + foobar xs
+  | otherwise = foobar xs
+
+foobar' :: [Integer] -> Integer
+foobar' = sum . map (\x -> 7*x + 2) . filter (>3)
+
+length' :: [a] -> Int
+length' []     = 0
+length' (_:xs) = 1 + length' xs
+
+-- class of things which can be converted to a list of Ints
+class Listable a where
+  toList :: a -> [Int]
+
+instance Listable Int where
+  -- toList :: Int -> [Int]
+  toList x = [x]
+
+instance Listable Bool where
+  toList True = [1]
+  toList False = [0]
+
+instance Listable [Int] where
+  toList = id
+
+data Tree a = Empty | Node a (Tree a) (Tree a)
+
+instance Listable (Tree Int) where
+  toList Empty = []
+  toList (Node x l r) = toList l ++ [x] ++ toList r
+
+sumL x = sum (toList x)
