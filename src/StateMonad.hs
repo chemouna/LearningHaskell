0733178519b2f1cc8a5674084e9196d3de7afa3e
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-05-08 23:39:20 +0200

    Examples of State Monad.

--- src/StateMonad.hs
+++ src/StateMonad.hs
@@ -2,3 +2,29 @@ module StateMonad where
 
 import Control.Monad
 import Control.Monad.State
+
+-- some State Monad examples
+
+type Stack = [Int]
+
+pop :: State Stack Int
+pop = state $ \(x:xs) -> (x, xs)
+
+push :: Int -> State Stack ()
+push a = state $ \xs -> ((), a:xs)
+
+swap :: State Stack ()
+swap = do a <- pop
+          b <- pop
+          push a
+          push b
+
+dup :: State Stack ()
+dup = do a <- pop
+         push a
+         push a
+
+drop :: State Stack ()
+drop = do pop
+          a <- pop
+          push a
