fb6722d812e0fb7112d68ec988f65749d5400776
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-05-09 05:24:02 +0200

    Add examples.

--- src/StateMonad.hs
+++ src/StateMonad.hs
@@ -5,6 +5,8 @@ import Control.Monad.State
 
 -- some State Monad examples
 
+-- Stack
+
 type Stack = [Int]
 
 pop :: State Stack Int
@@ -31,4 +33,29 @@ drop = do pop
 
 changeStack :: State Stack Int
 changeStack = push 3 >> push 2 >> push 3 >>= \_ -> pop
+
+
+-- Sequence
+
+data Seq = Seq String Int
+
+instance Show Seq where
+  show (Seq s i) = s ++ show i
+
+instance Enum Seq where
+  succ (Seq s i) = Seq s $ succ i
+  pred (Seq s i) = Seq s $ pred i
+  toEnum i = Seq "" i
+  fromEnum (Seq s i) = i
+
+nextSeq :: (Enum s, MonadState s m) => m s
+nextSeq = do s <- get
+             put (succ s)
+             return s
+
+-- nextSeq = get >>= put succ return
+
+next :: (Show s, Enum s, MonadState s m) => String -> m String
+next pre = nextSeq >>= \s -> return (pre ++ show s)
+
 
