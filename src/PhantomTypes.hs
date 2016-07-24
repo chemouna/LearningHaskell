e6b592cbd6c51959f5dfab2419fa7b37b5de682c
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-07-24 03:39:13 +0200

    Add examples with Phantom types and data kinds.

--- src/PhantomTypes.hs
+++ src/PhantomTypes.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DataKinds #-}
 
 module PhantomTypes where
 
@@ -40,3 +43,35 @@ instance Sanitise Validated where
 
 instance Sanitise Unvalidated where
   sanitise (FormData str) = FormData (filter isAlpha str)
+
+-----------------
+-- | Example 2
+-----------------
+
+newtype Distance a = Distance Double deriving (Num, Show)
+data Kilometer
+data Mile
+
+marathonDistance :: Distance Kilometer
+marathonDistance = Distance 42.25
+
+distanceKmToMilles :: Distance Kilometer -> Distance Mile
+distanceKmToMilles (Distance km) = Distance (0.62 * km)
+
+marathonDistanceInMiles :: Distance Mile
+marathonDistanceInMiles = distanceKmToMilles marathonDistance
+
+-- using DataKind to restrict to only a set of units
+data LengthUnit = Kilometer | Mile
+
+newtype Distance2 (a :: LengthUnit) = Distance2 Double
+  deriving (Num, Show)
+
+marathonDistance2 :: Distance2 'Kilometer
+marathonDistance2 = Distance2 42.25
+
+distanceKmToMilles2 :: Distance2 'Kilometer -> Distance2 'Mile
+distanceKmToMilles2 (Distance2 km) = Distance2 (0.62 * km)
+
+marathonDistanceInMiles2 :: Distance2 'Mile
+marathonDistanceInMiles2 = distanceKmToMilles2 marathonDistance2
