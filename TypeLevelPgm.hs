8e1919d3f711a27d0a903b477ddf840a6ced4abf
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-08-23 07:00:21 +0200

    Using type families.

--- TypeLevelPgm.hs
+++ TypeLevelPgm.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, EmptyDataDecls, UndecidableInstances #-}
 
@@ -15,3 +16,7 @@ class Plus m n r | m n -> r
 instance Plus Z n n
 instance (Plus m n r) => Plus (S m) n (S r)
 
+-- using Type Families
+type family Plus2 m n :: *
+type instance Plus2 Z n = n
+type instance Plus2 (S m) n = S (Plus2 m n)
