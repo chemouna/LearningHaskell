9ae8d68b746cc125de41c7711b98c2ab9c1dee37
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-08-30 07:02:40 +0200

    Add experiments with Data.Array functions.

--- src/ExploreArrays.hs
+++ src/ExploreArrays.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE TemplateHaskell #-}
 module ExploreArrays where
 
 import Data.Array
+import qualified Data.Map.Strict as M
 
+squares :: Array Integer Integer
 squares = array (1,100) [(i, i*i) | i <- [1..100]]
 
 mkArray :: (Ix a) => (a -> b) -> (a,a) -> Array a b
 mkArray f bnds = array bnds [(i, f i ) | i <- range bnds]
 
+squares' :: Array Integer Integer
 squares' = mkArray (\i -> i*i) (1,100)
 
 fibs :: Int -> Array Int Int
@@ -20,10 +24,34 @@ test = a where a = array (1,100) ([(1,1)] ++ [(i, i * a!(i-1)) | i <- [2..100]])
 swapRows :: (Ix a, Ix b, Enum b) => a -> a -> Array (a,b) c -> Array (a,b) c
 swapRows i i' a = a // ([((i, j), a!(i', j)) | j <- [jLo..jHi]] ++
                         [((i', j), a!(i,j)) | j <- [jLo..jHi]])
-                  where ((iLo, jLo), (iHi, jHi)) = bounds a
+                  where ((_, jLo), (_, jHi)) = bounds a
 
 swapRows2 :: (Ix a, Ix b, Enum b) => a -> a -> Array (a,b) c -> Array (a,b) c
 swapRows2 i i' a = a // [assoc | j <- [jLo..jHi],
                                  assoc <- [((i,j), a!(i',j)),
                                            ((i',j), a!(i,j))]]
-                   where ((iLo, jLo), (iHi,jHi)) = bounds a
+                   where ((_, jLo), (_,jHi)) = bounds a
+
+
+hist :: (Ix a, Num b) => (a,a) -> [a] -> Array a b
+hist bnds is = accumArray (+) 0 bnds [(i,1) | i <- is , inRange bnds i]
+
+testHist a1 = hist (0,10) (concat $ take 2 $ repeat [1..20])
+
+testHist2 a1 = hist (0,10) (concat $ replicate 2 [1..20])
+
+
+-- sorting a list by counting the nb of time each element occurs
+
+count :: [Int] -> Int -> Array Int Int
+count xs m = accumArray (+) 0 (0,m) (zip xs (repeat 1))
+
+sort xs m = concat [replicate c x | (x,c) <- assocs (count xs m)]
+
+-- Array counters
+arrcount :: [Int] -> M.Map Int Int
+arrcount xs = M.fromListWith (+) $ zip xs (repeat 1)
+-- arrcount can be written with accumArray
+arrcount2 :: [Int] -> Array Int Int
+arrcount2 xs = accumArray (+) 0 bnds [(i,1) | i <- xs, inRange bnds i]
+                 where bnds = (1, (maximum xs))
