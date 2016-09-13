10edd79303f8b27b4611b1d146eb1e17c4c448c6
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-09-13 07:01:49 +0200

    Different ways of writing zipMap.

--- src/Exploring.hs
+++ src/Exploring.hs
@@ -10,3 +10,23 @@ prob1_sol1  = sum [x | x <- takeWhile (<= 1000000) fibs, even x]
 
 -- factorial n = factorial (n - 1) * n
 
+zipMap1 :: [a -> b] -> [a] -> [b]
+zipMap1 [] _ = []
+zipMap1 _ [] = []
+zipMap1 (f:fs)(x:xs) = (f x):(zipMap1 fs xs)
+
+zipMap2 :: [a -> b] -> [a] -> [b]
+zipMap2 fs xs = map fstOnSnd (zip fs xs)
+  where fstOnSnd (f,x) = f x
+
+zipMap3 :: [a -> b] -> [a] -> [b]
+zipMap3 = zipWith ($)
+
+-- and applying combinators : S = zipMap and K = repeat we can write map, zip, zipWith, zipWith3
+s = zipWith ($)
+k = repeat
+
+mapT f xs = s (k f) xs
+zipWithT f as bs     = s (mapT f as) bs
+zipT                 = zipWithT (,)
+zipWith3T f as bs cs = s (zipWithT f as bs) cs
