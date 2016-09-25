e7aa300b5c87687d6ab59bc9ef81d616af961c85
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-09-25 22:20:19 +0200

    Suite let usage.

--- src/LetUsage.hs
+++ src/LetUsage.hs
@@ -1,6 +1,7 @@
-
 module LetUsage where
 
+import Control.Monad.Writer
+import Control.Monad
 import Prelude
 
 -- example of different of writing a fn using let .. in .. and where
@@ -26,10 +27,53 @@ solveLogicW a b
 -- another way using case
 solveLogicC :: Int -> Int -> Int
 solveLogicC a b =
-  let x = 1
-      case () of
-        _ | a >= x -> 1
-          | a == b -> 333
-          | otherwise -> 5
+    case () of
+      _ | a >= x -> 1
+        | a == b -> 33
+        | otherwise -> 5
+     where x = 1
 
+-- Using if in a let expression
 
+-- foo :: Integer -> Integer -> [Integer]
+-- foo a b = do
+--             let result = []
+--             let Coord x y = boo a b
+--             if x > 0
+--               let result = result ++ [3]
+--             if y > 0
+--               let result = result ++ [5]
+--             if x < a
+--               let result = result ++ [7]
+--             if y < b
+--               let result = result ++ [9]
+--             result
+-- the above foo function doesn't work -> a better way to write it
+
+data Coord a b = Coord a b
+boo = undefined
+
+foo :: Integer -> Integer -> [Integer]
+foo a b = map snd
+           (filter fst [(x > 0, 3),
+             (y > 0, 5),
+             (x < a, 7),
+             (y < b, 9)])
+      where Coord x y = boo a b
+
+-- another way
+foo2 a b = let Coord x y = boo a b in
+           (if x > 0 then (3 :) else id) .
+           (if y > 0 then (5 :) else id) .
+           (if x < a then (7 :) else id) .
+           (if y < b then (9 :) else id) $
+           []
+
+-- with the writer monad
+foo3 a b = execWriter $ do
+             let Coord x y = boo a b
+             when (x > 0) $ tell [3]
+             when (y > 0) $ tell [5]
+             when (x < a) $ tell [7]
+             when (y < b) $ tell [9]
+t
