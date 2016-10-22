731d91644a5d63c32c8e349588f675ba757178b6
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-10-23 01:43:35 +0200

    suite.

--- src/Explore3.hs
+++ src/Explore3.hs
@@ -1,5 +1,6 @@
 module Explore3 where
 
+import Control.Monad
 import Test.QuickCheck
 import Test.QuickCheck.All
 
@@ -16,4 +17,16 @@ prop_f' = forAll ((,) <$> r1 <*> r2) $ \(x,y) -> f x y
   where
     r1 = choose (0, 10)
     r2 = choose (10, 20)
+
+-- or by defining our own new types
+newtype R1 = R1 Int deriving Show
+newtype R2 = R2 Int deriving Show
+
+instance Arbitrary R1 where
+  arbitrary = R1 <$> choose (0, 10)
+
+instance Arbitrary R2 where
+  arbitrary = R2 <$> choose (10, 20)
+
+prop_f'' (R1 x) (R2 y) = f x y
 
