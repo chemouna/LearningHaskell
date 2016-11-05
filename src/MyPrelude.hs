05194544f94fa8f26d24fb8f748fce197ef047f4
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-11-05 07:25:45 +0100

    implementation of myDeleteBy

--- src/MyPrelude.hs
+++ src/MyPrelude.hs
@@ -8,3 +8,8 @@ xs `myindex` k | k < 0 = error "Cannot get a value for a negative index"
 [] `myindex` _         =  error "Prelude.!!: index too large"
 (x:_) `myindex` 0      = x
 (_:xs) `myindex` k     = xs `myindex` (k - 1)
+
+
+myDeleteBy :: (a -> a -> Bool) -> a -> [a] -> [a]
+myDeleteBy _ _ [] = []
+myDeleteBy eq v (x:xs) = if v `eq` x then xs else x : myDeleteBy eq v xs
