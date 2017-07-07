dc95e1aef8661f82e893c7d8451b4a63f2dcfebd
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-07-07 17:48:26 +0000

    Starting to explore bloom filters and other modifications.

---
+++ BloomFilter.hs
@@ -0,0 +1,10 @@
+module BloomFilterExplore where 
+
+-- |
+--- Bloom Filter Data structure : 
+--  a space-efficient probabilistic daya structure, used to test whether an element is in a data set
+--  a query on a bloom filter returns either "possibly in set" or "definitely not in set". 
+
+
+
+
---
+++ src/FenwickTree.hs
@@ -0,0 +1,4 @@
+
+module FenwickTree where
+
+
