f99a3bdfab56c8378ae5475d67df08f1bfb543d0
Mouna Cheikhna <cheikhnamouna@dyn911-162.guest.ic.ac.uk>
2017-08-13 21:49:33 +0100

    Typeclasses

--- src/ExploreTypesClasses.hs
+++ src/ExploreTypesClasses.hs
@@ -2,9 +2,37 @@
 
 module ExploreTypeClasses where
 
-class PolyParam where
-  op :: Int -> Int
 
+class CanAdd a where
+  add :: a -> a -> a
+
+instance CanAdd Int where
+  add x y = x + y
+
+instance CanAdd Float where
+  add x y = x + y
+
+instance CanAdd a => CanAdd (Maybe a) where
+  add mx my =
+    case (mx, my) of
+      (Just x, Just y) -> Just (add x y)
+      _ -> Nothing
+
+class Mappable container where
+  map :: (a -> b) -> container a -> container b
+
+instance Mappable [] where
+  map f list =
+    case list of
+      [] -> []
+      x:xs -> f x : ExploreTypeClasses.map f xs
+
+instance Mappable Maybe where
+  map f maybe =
+    case maybe of
+      Just x -> Just (f x)
+      Nothing -> Nothing
+      
 
 
 
