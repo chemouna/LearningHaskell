b4cee52dd4818c28f3785aa1bb4180c249f91570
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-10 07:55:32 +0100

    Add imperative heap implementation.

--- src/ImperativeHeaps.hs
+++ src/ImperativeHeaps.hs
@@ -1,4 +1,3 @@
-
 module ImperativeHeaps where
 
 
@@ -20,4 +19,17 @@ siftup' :: Ord a => Int -> V.Vector a -> V.Vector a
 siftup' i heap | i == 1 || heap ! j <= heap ! i = heap
                | otherwise = siftup j $ swap i j heap
                where j = div i 2
-               
+
+siftdown :: Ord a => Int -> V.Vector a -> V.Vector a
+siftdown n h = f 1 h where
+  f i heap = if 2 * i > n || heap ! i <= c
+             then heap
+             else f j $ swap i j heap
+    where (c, j) = minimum [(heap ! x, x) | x <- [2 * i, 2 * i + 1], x <= n]
+
+
+hsort :: Ord a => V.Vector a -> V.Vector a
+hsort heap = foldr (\i -> siftdown (i - 1) . swap 1 i) (foldl (flip siftup) heap [2..V.length heap]) [2..V.length heap]
+
+main :: IO ()
+main = print $ hsort (V.fromList [4,7,8,1,5,3,2,9,6]) == V.fromList [9..1]
