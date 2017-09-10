3ba3346453add60b1b54eb2e1f74c3a80f14f22e
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-10 03:56:09 +0100

    Add siftup.

--- src/ImperativeHeaps.hs
+++ src/ImperativeHeaps.hs
@@ -11,4 +11,13 @@ v ! i = v V.! (i - 1)
 swap :: Ord a => Int -> Int -> V.Vector a -> V.Vector a
 swap i j heap  = heap V.// [(i-1, heap ! j), (j-1, heap ! i)]
 
+siftup :: Ord a => Int -> V.Vector a -> V.Vector a
+siftup i heap = let j = div i 2
+                    in if i == 1 || heap ! j <= heap ! i
+                       then heap else siftup j $ swap i j heap
 
+siftup' :: Ord a => Int -> V.Vector a -> V.Vector a
+siftup' i heap | i == 1 || heap ! j <= heap ! i = heap
+               | otherwise = siftup j $ swap i j heap
+               where j = div i 2
+               
