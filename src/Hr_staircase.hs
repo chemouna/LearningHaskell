f9fe3111afddb908dc18be0bd6162f15b4149552
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-20 22:47:19 +0100

    A working solution.

--- src/Hr_staircase.hs
+++ src/Hr_staircase.hs
@@ -13,23 +13,29 @@ import Test.Hspec
 ####
 -}
 
+pad :: Char -> [String] -> [String]
+pad c xs = ys where
+  (ys, n)         = foldr cons ([],0) xs
+  cons x (acc, m) = ((replicate (n - m') c ++ x) : acc, max m m')
+    where m' = length x
+
 solution :: Int -> [String]
 solution 0 = []
-solution n = map (\x -> take x (repeat '#')) [1..n] 
+solution n = pad ' ' (map (\x -> take x (repeat '#')) [1..n])
+
 
 main :: IO ()
 main = do
     n_temp <- getLine
     let n = read n_temp :: Int
-    print n
+    mapM_ print (solution n)
 
 spec :: Spec
 spec =
   describe "Solution to Staircase problem" $ do
     it "Special Case for n = 0" $ do
       solution 0 `shouldBe` []
-    it "Special case for
-    n = 1" $ do
+    it "Special case for n = 1" $ do
       solution 1 `shouldBe` ["#"]
     it "Case n = 2" $ do
       solution 2 `shouldBe` ["#", "##"]
