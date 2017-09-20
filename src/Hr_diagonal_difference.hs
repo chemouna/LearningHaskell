8eec7ea29de85b7a3d0d336447bfe44a39e1e9be
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-20 06:39:55 +0100

    Get rid of requirement to tuple select.

--- src/Hr_diagonal_difference.hs
+++ src/Hr_diagonal_difference.hs
@@ -1,14 +1,14 @@
 module Hr_Diagonal_difference where
 
-import Data.Tuple.Select
 
 solution list n = abs(sum_diag1 - sum_diag2)
   where
     sum_diag1 = sum (zipWith (\ x y -> x !! y) list [0..n-1])
     sum_diag2 = sum (zipWith (\ x y -> x !! y) list (enumFromThenTo (n-1) 1 0))
 
-solution2 list n = abs (sel1 p - sel2 p)
+solution2 list n = abs (p1 - p2)
   where
+    (p1, p2) = p
     p = foldl (\ acc@(x', y') (x,y) -> (x' + x, y' + y)) (0,0) pairs
     pairs = zipWith3 (\ x y z -> (x !! y, x !! z)) list [0..n-1]  (enumFromThenTo (n-1) 1 0)
 
