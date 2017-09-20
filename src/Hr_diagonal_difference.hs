1062da5a25afc2020c9dccc030e06ddb78067e0a
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-20 06:36:22 +0100

    Attempt with a different solution to improve speed.

--- src/Hr_diagonal_difference.hs
+++ src/Hr_diagonal_difference.hs
@@ -1,10 +1,16 @@
 module Hr_Diagonal_difference where
 
+import Data.Tuple.Select
+
 solution list n = abs(sum_diag1 - sum_diag2)
   where
     sum_diag1 = sum (zipWith (\ x y -> x !! y) list [0..n-1])
     sum_diag2 = sum (zipWith (\ x y -> x !! y) list (enumFromThenTo (n-1) 1 0))
 
+solution2 list n = abs (sel1 p - sel2 p)
+  where
+    p = foldl (\ acc@(x', y') (x,y) -> (x' + x, y' + y)) (0,0) pairs
+    pairs = zipWith3 (\ x y z -> (x !! y, x !! z)) list [0..n-1]  (enumFromThenTo (n-1) 1 0)
 
 getMultipleLines :: Int -> IO [String]
 getMultipleLines n
@@ -22,4 +28,5 @@ main = do
     let n = read n_temp :: Int
     a_temp <- getMultipleLines n
     let a = map ( map ( read :: String -> Int ) . words ) a_temp
-    print $ solution a n
+    print a
+    print $ solution2 a n
