da9312db55e0449cb605b0ff96a850e5d270b9d5
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-20 22:39:56 +0100

    First draft of a solution.

--- src/Hr_staircase.hs
+++ src/Hr_staircase.hs
@@ -3,6 +3,7 @@ module Hr_staircase where
 import Control.Applicative
 import Control.Monad
 import System.IO
+import Test.Hspec
 
 
 {-
@@ -13,10 +14,24 @@ import System.IO
 -}
 
 solution :: Int -> [String]
-solution n = 
+solution 0 = []
+solution n = map (\x -> take x (repeat '#')) [1..n] 
 
 main :: IO ()
 main = do
     n_temp <- getLine
     let n = read n_temp :: Int
     print n
+
+spec :: Spec
+spec =
+  describe "Solution to Staircase problem" $ do
+    it "Special Case for n = 0" $ do
+      solution 0 `shouldBe` []
+    it "Special case for
+    n = 1" $ do
+      solution 1 `shouldBe` ["#"]
+    it "Case n = 2" $ do
+      solution 2 `shouldBe` ["#", "##"]
+    it "Case n = 4" $ do
+      solution 4 `shouldBe` ["#", "##", "###", "####"]
