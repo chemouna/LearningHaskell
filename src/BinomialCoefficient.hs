efaf5c157ef3a194793afa8ea80005aa8134ea1e
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-21 22:58:21 +0100

    Another recursive solution.

--- src/BinomialCoefficient.hs
+++ src/BinomialCoefficient.hs
@@ -5,3 +5,8 @@ module BinomialCoefficient where
 fact 0 = 1
 fact n = n * fact (n - 1)
 choose n k = (fact n) `div` ((fact k)*(fact (n-k)))
+
+-- another solution using recurrence
+choose' n 0 = 1
+choose' 0 k = 0
+choose' n k = (choose (n - 1) (k - 1)) * (n `div` k)
