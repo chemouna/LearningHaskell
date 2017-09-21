94f682d19883d52db021f915f688e2442580bd12
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-21 07:49:23 +0100

    Fix for solution to pass the test.

--- src/Hr_mini_max_sum.hs
+++ src/Hr_mini_max_sum.hs
@@ -6,7 +6,6 @@ import Data.List (splitAt)
 deleteAt idx xs = lft ++ rgt
   where (lft, (_:rgt)) = splitAt idx xs
 
--- foldr (\x (acc, i) -> (acc ++ (sum (deleteAt i xs)), i + 1) ([], 0) [1,2,3,4]
 solution :: [Int] -> (Int, Int)
 solution xs = (minimum sums, maximum sums)
   where
@@ -15,15 +14,11 @@ solution xs = (minimum sums, maximum sums)
     consSum :: Int -> ([Int], Int) -> ([Int], Int)
     consSum x (acc, i) = ((sum (deleteAt i xs)) : acc, i + 1)
 
-showTup :: (Show a, Show b) => (a,b) -> String
-showTup (a,b) = (show a) ++ " " ++ (show b)
-
 main :: IO ()
 main = do
     arr_temp <- getLine
     let arr = map read $ words arr_temp :: [Int]
-    print $ showTup (solution arr)
-      -- $ uncurry (\x y -> ) solution arr
+    putStr ( unwords (map show (uncurry (\x y -> [x, y]) (10,14))))
 
 getMultipleLines :: Int -> IO [String]
 getMultipleLines n
