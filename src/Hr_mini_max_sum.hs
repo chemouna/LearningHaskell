3ddb17f923e223d1be9891dcf34e59c1b9394756
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-21 07:54:02 +0100

    Solution 3

--- src/Hr_mini_max_sum.hs
+++ src/Hr_mini_max_sum.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Hr_mini_max_sum where
 
-import Data.List (splitAt)
+import Data.List (splitAt, sort)
 
 deleteAt idx xs = lft ++ rgt
   where (lft, (_:rgt)) = splitAt idx xs
@@ -23,7 +23,34 @@ main = do
 -- Solution 2
 main2 = fmap (map (\x -> read x :: Int).words) getLine >>= (putStr.unwords.(map show).(\l -> [sum l - maximum l, sum l - minimum l]))
 
--- S
+-- Solution 3
+main3 :: IO ()
+main3 = do
+    arr_temp <- getLine
+    let arr = map read $ words arr_temp :: [Integer]
+    let numCombos = combos 4 arr
+    let sortedSums = sort $ map sum numCombos
+    let minN = show $ head sortedSums
+    let maxN = show $ head $ reverse sortedSums
+    putStrLn $ minN ++ " " ++ maxN
+
+
+combos :: Int -> [a] -> [[a]]
+combos n xs =
+  if length xs == n
+  then [xs]
+  else case xs of
+        []    -> []
+        (x:t) -> sprinkle x (combos (n-1) t) ++ combos n t
+
+sprinkle :: a -> [[a]] -> [[a]]
+sprinkle x ys =
+  case ys of
+    []    -> []
+    (y:t) -> (x:y) : sprinkle x t
+
+--
+
 getMultipleLines :: Int -> IO [String]
 getMultipleLines n
     | n <= 0 = return []
