c233cb2dd456787d26d6622ad2ce8259116537fd
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-10-21 13:39:36 +0100

    Fixing some test cases.

--- src/practice/topcoder/Whisper.hs
+++ src/practice/topcoder/Whisper.hs
@@ -7,26 +7,38 @@ import Data.List.Split
 import Data.List
 import Data.Ord
 import Test.Hspec
+import Data.Char
 
 -- String toWhom(String[] usernames, String typed)
 
 toWhom :: [String] -> String -> String
 toWhom us m =
-  if strStartsWith m "/msg " then  findWhom us (m \\ msg)
+  if strStartsWith ml "/msg " then findWhom us (diffLowerCase m msg)
   else  "not a whisper"
   where
     msg = "/msg "
+    ml = strToLower m
 
 findWhom :: [String] -> String -> String
 findWhom us m
   | null candidates = "user is not logged in"
   | otherwise = maximumBy (comparing length) candidates
   where
-    candidates = ll `intersect` us
+    candidates = intersectBy (\x y -> strToLower x == strToLower y) ll us
     s = splitOn " " m
     r = tail $ subsequences s
     z = filter (\(x:xs) -> x == head s) r
     ll = map (concat . intersperse " ") z
+
+diffLowerCase :: [Char] -> [Char] -> [Char]
+diffLowerCase =  foldl (flip deleteByLower)
+
+deleteByLower             ::  Char -> [Char] -> [Char]
+deleteByLower _ []     = []
+deleteByLower x (y:ys) = if x' == y' then ys else y : deleteByLower x ys
+  where
+    x' = toLower x
+    y' = toLower y
 
 
 main :: IO ()
@@ -34,15 +46,15 @@ main = hspec $ do
   describe "toWhom" $ do
     it "Case 1" $ do
       toWhom ["John","John Doe","John Doe h"] "/msg John Doe hi there" `shouldBe` "John Doe"
+
     it "Case 2" $ do
       toWhom ["John","John Doe","John Doe h"] "/MSG jOHN dOE HI THERE" `shouldBe` "John Doe"
 
     it "Case 3" $ do
-      toWhom ["writer"] "writer hi" `shouldBe` "John Doe"
+      toWhom ["writer"] "writer hi" `shouldBe` "not a whisper"
 
     it "Case 4" $ do
       toWhom ["tester"] "/msg testerTwo you there" `shouldBe` "user is not logged in"
-
 
     it "Case 5" $ do
       toWhom ["lbackstrom"] "/msg lbackstrom" `shouldBe` "user is not logged in"
