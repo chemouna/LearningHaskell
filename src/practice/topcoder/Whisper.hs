f77e6b6ae1fcb01a6a8bab60ab52ced523d678c9
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-10-21 13:04:46 +0100

    A working solution.

--- src/practice/topcoder/Whisper.hs
+++ src/practice/topcoder/Whisper.hs
@@ -1,21 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 
 module Whisper where
 
+import Data.Strings
 import Data.List.Split
+import Data.List
+import Data.Ord
+
 
 -- String toWhom(String[] usernames, String typed)
 
-{--
 toWhom :: [String] -> String -> String
-toWhom
-  | (msg:rest) = findWhom rest
-  | "not a whisper"  
+toWhom us m =
+  if strStartsWith m "/msg " then  findWhom us (m \\ msg)
+  else  "not a whisper" 
   where
     msg = "/msg "
-    findWhom rest
-     | () 
---}
+
+findWhom :: [String] -> String -> String
+findWhom us m = maximumBy (comparing length) $ ll `intersect` us 
+  where
+    s = splitOn " " m
+    r = tail $ subsequences s
+    z = filter (\(x:xs) -> x == head s) r
+    ll = map (concat . intersperse " ") z
+
+
 {--
 
 let us = ["John","John Doe","John Doe h"]
@@ -29,10 +40,6 @@ filter (\(x:xs) -> x == head r) (tail s)
 
 init $ tail $ splitOn " " m -- TODO: instead of just init check if there a space at the end
 -- Fix for a case like "/msg John Doe "
-
-let r = tail $ subsequences s
-
-let z = filter (\(x:xs) -> x == head s) r
 
 let ll = map (concat . intersperse " ") z
 
