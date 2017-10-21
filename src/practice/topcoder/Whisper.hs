efa98d57165c167d7a819445763975f7d3f22dff
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-10-21 13:18:56 +0100

    Adding some test cases for Whisper.

--- src/practice/topcoder/Whisper.hs
+++ src/practice/topcoder/Whisper.hs
@@ -1,25 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-
 module Whisper where
 
 import Data.Strings
 import Data.List.Split
 import Data.List
 import Data.Ord
-
+import Test.Hspec
 
 -- String toWhom(String[] usernames, String typed)
 
 toWhom :: [String] -> String -> String
 toWhom us m =
   if strStartsWith m "/msg " then  findWhom us (m \\ msg)
-  else  "not a whisper" 
+  else  "not a whisper"
   where
     msg = "/msg "
 
 findWhom :: [String] -> String -> String
-findWhom us m = maximumBy (comparing length) $ ll `intersect` us 
+findWhom us m = maximumBy (comparing length) $ ll `intersect` us
   where
     s = splitOn " " m
     r = tail $ subsequences s
@@ -27,23 +26,34 @@ findWhom us m = maximumBy (comparing length) $ ll `intersect` us
     ll = map (concat . intersperse " ") z
 
 
-{--
+main :: IO ()
+main = hspec $ do
+  describe "toWhom" $ do
+    it "Case 1" $ do
+      toWhom ["John","John Doe","John Doe h"] "/msg John Doe hi there" `shouldBe` "John Doe"
+    it "Case 2" $ do
+      toWhom ["John","John Doe","John Doe h"] "/MSG jOHN dOE HI THERE" `shouldBe` "John Doe"
 
-let us = ["John","John Doe","John Doe h"]
-let m = "/msg John Doe hi there"
+    it "Case 3" $ do
+      toWhom ["writer"] "writer hi" `shouldBe` "John Doe"
 
- strStartsWith m "/msg "
 
-tail $ splitOn " " m
 
-filter (\(x:xs) -> x == head r) (tail s)
+    it "Case 4" $ do
+      toWhom ["tester"] "/msg testerTwo you there" `shouldBe` "user is not logged in"
 
-init $ tail $ splitOn " " m -- TODO: instead of just init check if there a space at the end
--- Fix for a case like "/msg John Doe "
 
-let ll = map (concat . intersperse " ") z
+    it "Case 5" $ do
+      toWhom ["lbackstrom"] "/msg lbackstrom" `shouldBe` "user is not logged in"
 
--> maximumBy (comparing length) $ ll `intersect` us
+    it "Case 6" $ do
+      toWhom ["me"] "/msg me hi" `shouldBe` "user is not logged in"
 
-TODO: handle lowercase 
--}
+    it "Case 7" $ do
+      toWhom ["abc"] " /msg abc note the leading space" `shouldBe` "not a whisper"
+
+    it "Case 8" $ do
+      toWhom ["Wow"]"/msg Wow " `shouldBe` "Wow"
+
+    it "Case 9" $ do
+      toWhom ["msg"] "/msg" `shouldBe`  "not a whisper"
