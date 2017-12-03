ffe82c3506dae419f2a5fd5bb2c19bdac64025f7
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-12-03 21:22:29 +0000

    Some cleanup.

---
+++ src/cyclefn.hs
@@ -0,0 +1,21 @@
+
+module Cyclefn where
+
+
+{--
+take 7 $ cycle [1..4]
+
+-}
+
+
+{--
+ Example: Euler 35 : Circular primes
+
+ https://projecteuler.net/problem=35
+ The number, 197, is called a circular prime because all
+ rotations of the digits: 197, 971, and 719, are themselves
+ prime.
+
+ How many circular primes are there below one million ?
+
+-}
