88752e5d5f6aa919b576854117d5411a5745db5f
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-12-03 20:50:10 +0000

    On ZipWith.

--- src/ZipWith.hs
+++ src/ZipWith.hs
@@ -1,6 +1,5 @@
 module ZipWith where
 
-
 {--
 
 zipWith calls the given function pairwise on each member of both lists. So zipWith f [a,b,c] [x,y,z]
@@ -20,7 +19,7 @@ expl1 = [zipWith (*) [1,2,3] [3,2,2],
 divlist :: Integral a => [a] -> [a] -> [Maybe a]
 divlist = zipWith (\x y -> if (y /= 0) then Just (x `div` y) else Nothing)
 
---  divlist [3, 4, 6] [2, 0, 3]
+-- divlist [3, 4, 6] [2, 0, 3]
 
 
 -- fibonacci:
@@ -55,14 +54,13 @@ pascal = iterate next [1]
 next2 xs = [1] ++ zipWith (+) xs (tail xs) ++ [1]
 
 
-
 {--
 Given an array of numbers, nums, return an array of numbers products, where products[i] is the product of
 all nums[j], j != i.
 
-Input : [1, 2, 3, 4, 5]
-Output: [(2*3*4*5), (1*3*4*5), (1*2*4*5), (1*2*3*5), (1*2*3*4)]
-      = [120, 60, 40, 30, 24]
+Input : [1, 2, 3, 4]
+Output: [(2*3*4), (1*3*4), (1*2*4), (1*2*3)]
+      = [120, 60, 40, 30]
 
 do this in O(N) without using division.
 
