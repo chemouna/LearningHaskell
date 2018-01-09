39f15d0316439c6edacb31a2685bd149854c204c
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-01-09 00:37:03 +0000

    lcm.

--- src/Euclid.hs
+++ src/Euclid.hs
@@ -4,3 +4,10 @@ module Euclid where
 gcd' :: (Integral a) => a -> a -> a
 gcd' a 0 = a
 gcd' a b = gcd' b (a `mod` b)
+
+
+lcm' :: (Integral a) => a -> a -> a
+lcm' _ 0 = 0
+lcm' 0 _ = 0
+lcm' x y = abs ((x `quot` (gcd x y)) * y)
+
